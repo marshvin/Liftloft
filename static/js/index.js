18f6ad4fb7bdd98f0598c0e99d24072c85627acf
marshvin <vincentmarsh01@gmail.com>
2024-02-12 01:15:36 +0300

    Added a spinner

--- static/js/index.js
+++ static/js/index.js
@@ -1,50 +1,14 @@
-// Burger menus
-document.addEventListener('DOMContentLoaded', function() {
-    // open
-    const burger = document.querySelectorAll('.navbar-burger');
-    const menu = document.querySelectorAll('.navbar-menu');
+document.getElementById('openModal').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default action (e.g., form submission)
 
-    if (burger.length && menu.length) {
-        for (var i = 0; i < burger.length; i++) {
-            burger[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-
-    // close
-    const close = document.querySelectorAll('.navbar-close');
-    const backdrop = document.querySelectorAll('.navbar-backdrop');
-
-    if (close.length) {
-        for (var i = 0; i < close.length; i++) {
-            close[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-
-    if (backdrop.length) {
-        for (var i = 0; i < backdrop.length; i++) {
-            backdrop[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
+    // Toggle the visibility of the modal
+    document.getElementById('modal').classList.toggle('hidden');
 });
 
-    // Open modal
-    document.getElementById("openModal").onclick = function openModal() {
-        document.getElementById("modal").style.display = "block";
-    };
+document.getElementById('closeModal').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default action (e.g., form submission)
 
-    // Close modal
-    document.getElementById("closeModal").onclick = function closeModal() {
-        document.getElementById("modal").style.display = "none";
-    };
+    // Close the modal
+    document.getElementById('modal').classList.add('hidden');
+});
+
